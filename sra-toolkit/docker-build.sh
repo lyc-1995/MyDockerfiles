779fcb108490badf8c32ca565d9f25c6bb94a836
lyc-1995 <liyuchen_1995@outlook.com>
2020-11-24 12:56:16 +0800

    Create an image for sra-toolkit-2.10.8
    * Use lyc1995/ubuntu:20.04.1 as base image
    * Install sra-toolkit from https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.10.8/sratoolkit.2.10.8-ubuntu64.tar.gz
    * Manually configure sra-toolkit in an intermediate container

---
+++ sra-toolkit/docker-build.sh
@@ -0,0 +1,13 @@
+version=$1
+
+repository="sra-toolkit"
+
+cd $version
+wget -c https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/${version}/sratoolkit.${version}-ubuntu64.tar.gz
+
+docker build -t lyc1995/${repository}:${version} .
+docker run -it --name=sra-tmp lyc1995/${repository}:${version} && docker commit sra-tmp lyc1995/${repository}:${version}
+docker rm sra-tmp
+docker images | grep none | awk '{print $3}' | xargs docker rmi
+
+rm sratoolkit.${version}-ubuntu64.tar.gz
