6543f4c44398a8c6a947e0a31b22176b44b7681a
lyc-1995 <liyuchen_1995@outlook.com>
2021-09-10 11:37:05 +0800

    Update sra-toolkit
    sra-toolkit:2.11.0 -> 2.11.1
    Add ARG to set version during docker building

--- sra-toolkit/docker-build.sh
+++ sra-toolkit/docker-build.sh
@@ -2,11 +2,12 @@ version=$1
 
 repository="sra-toolkit"
 
-cd $version
+cd docker-build
 wget -c https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/${version}/sratoolkit.${version}-ubuntu64.tar.gz
 
-docker build -t lyc1995/${repository}:${version} .
-docker run -it --name=sra-tmp lyc1995/${repository}:${version} && docker commit sra-tmp lyc1995/${repository}:${version}
+docker build --build-arg VERSION=${version} -t lyc1995/${repository}:${version} .
+docker run -it --name=sra-tmp lyc1995/${repository}:${version} && \
+    docker commit sra-tmp lyc1995/${repository}:${version}
 docker rm sra-tmp
 docker images | grep none | awk '{print $3}' | xargs docker rmi
 
